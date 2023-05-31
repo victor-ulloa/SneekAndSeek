de94377fdf005707bf44081e322793a9098a3ed9
Alejandro Ulloa <alejandroekas@gmail.com>
2023-05-30 21:49:03 -0400

    project created

---
+++ Source/SneakAndSeek/SneakAndSeekGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SneakAndSeekGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNEAKANDSEEK_API ASneakAndSeekGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
